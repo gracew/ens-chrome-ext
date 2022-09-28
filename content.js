556b7907b04de169d83663ab8710e1bdb1ef24df
Grace Wang <gracewang92@gmail.com>
2022-09-27 22:47:43 -0700

    working :)

--- content.js
+++ content.js
@@ -1,2 +1,22 @@
+function replace(el) {
+    if (el.innerHTML.includes("0x")) {
+        var results = /0x[0-9A-Fa-f]{40}/.exec(el.innerHTML);
+        if (results && results.length > 0) {
+            var address = results[0];
+            provider.lookupAddress(address).then(resolved => {
+                if (resolved) {
+                    el.innerHTML = el.innerHTML.replace(address, resolved);
+                }
+            });
+
+        }
+    }
+}
+
 var provider = new ethers.providers.InfuraProvider("homestead", "a71874bbcb6a450398f24a7bbd436eda");
-provider.lookupAddress("0x06e6f7D896696167B2dA9281EbAF8a14580fbFCc").then(resolved => alert("resolved: " + resolved));
+
+var text = document.querySelectorAll('h1, h2, h3, h4, h5, p, li, td, caption, span, a');
+
+for (var el of text) {
+    replace(el);
+}
